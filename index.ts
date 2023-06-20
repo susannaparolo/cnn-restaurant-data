ba91e75c31d1f69c820290e31d7cb9c6b77992d5
Larissa <103778870+bobrovnikovaSelfridges@users.noreply.github.com>
2023-06-20 16:35:16 +0100

    feat(Devc-9) - preparing data (#1)
    * create init for preparing data
    
    * ?
    
    * host
    
    * save results
    
    * script for data preparation WIP
    
    * saved results to give Joanna a chance to solve it
    
    * Fix: return data and set types
    
    * resolved comments on PR
    
    ---------
    
    Co-authored-by: Larisa <94ilara@gmail.com>
    Co-authored-by: selfridges-joanna <joanna.bak@selfridges.co.uk>
    Co-authored-by: Chidera Chukwumah <cjchukwumah@yahoo.co.uk>

---
+++ index.ts
@@ -0,0 +1,54 @@
+import rawData from "./purchase-file.json";
+
+/**
+ * parse items into food & drink,
+ * split them
+ * get an array of objects
+ * key values is customer id
+ */
+
+type RawPurchaseDataType = {
+  customerId: string;
+  date: string;
+  items: string;
+};
+
+type FormattedDataUnit = {
+  customerId: Array<Purchase>;
+};
+
+type Purchase = {
+  date: string;
+  products: {
+    food: string[];
+    drinks: string[];
+  };
+};
+
+const parseData = () => {
+  const structuredData: any = {};
+
+  rawData.forEach((element: RawPurchaseDataType) => {
+    const allItems = element.items.split(",");
+    const food: Array<string> = [];
+    const drink: Array<string> = [];
+
+    allItems.forEach((item: string) => {
+      if (item.includes("FOOD")) {
+        food.push(item);
+      } else {
+        drink.push(item);
+      }
+    });
+    const purchase = {
+      date: element.date,
+      products: {
+        food,
+        drink,
+      },
+    };
+
+    structuredData[element.customerId] = [purchase];
+  });
+  return structuredData;
+};
