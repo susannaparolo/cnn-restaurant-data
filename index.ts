5ddc5d99e873a887b87f5bef18c80ef1530e0d6c
Chidera Chukwumah <cjchukwumah@yahoo.co.uk>
2023-06-20 16:22:23 +0100

    resolved comments on PR

--- index.ts
+++ index.ts
@@ -1,64 +1,54 @@
-import rawData from './purchase-file.json'
-
-
+import rawData from "./purchase-file.json";
 
 /**
  * parse items into food & drink,
  * split them
- * get an array of objects 
+ * get an array of objects
  * key values is customer id
  */
 
-
 type RawPurchaseDataType = {
-    customerId: string
-    date: string
-    items: string
-}
+  customerId: string;
+  date: string;
+  items: string;
+};
 
 type FormattedDataUnit = {
-    customerId: Array<Purchase>
-}
+  customerId: Array<Purchase>;
+};
 
 type Purchase = {
-    date: string
-    products: {
-        food: string[]
-        drinks: string[]
-    }
-
-}
-
+  date: string;
+  products: {
+    food: string[];
+    drinks: string[];
+  };
+};
 
 const parseData = () => {
-    const structuredData: any = {}
+  const structuredData: any = {};
 
-    rawData.forEach((element: RawPurchaseDataType) => {
-        const allItems = element.items.split(",")
-        const food: Array<string> = []
-        const drink: Array<string> = []
+  rawData.forEach((element: RawPurchaseDataType) => {
+    const allItems = element.items.split(",");
+    const food: Array<string> = [];
+    const drink: Array<string> = [];
 
-        allItems.forEach((item: string) => {
-            if (item.includes("FOOD")) {
-                food.push(item)
-            }
-            else {
-                drink.push(item)
-            }
-        })
-        const purchase = {
-            date: element.date,
-            products: {
-                food, drink
-            }
-        }
+    allItems.forEach((item: string) => {
+      if (item.includes("FOOD")) {
+        food.push(item);
+      } else {
+        drink.push(item);
+      }
+    });
+    const purchase = {
+      date: element.date,
+      products: {
+        food,
+        drink,
+      },
+    };
 
-        structuredData[element.customerId] = [purchase]
-
-
-    })
-    return structuredData
-}
-
-const result = parseData()
-console.log(result)
+    structuredData[element.customerId] = [purchase];
+  });
+  return structuredData;
+};
