260daab83d2c92e7ce5b6157cada857784754812
ChideraChukwumah100 <133105114+ChideraChukwumah100@users.noreply.github.com>
2023-06-27 15:40:03 +0100

    (Devc-21): added types (#2)
    * added types
    
    * remove console log
    
    * fixed formatting errors
    
    ---------
    
    Co-authored-by: Chidera Chukwumah <cjchukwumah@yahoo.co.uk>

--- index.ts
+++ index.ts
@@ -1,54 +1,36 @@
+import {
+  FormattedDataUnit,
+  Purchase,
+  RawPurchaseDataType,
+} from "./types/types";
+
 import rawData from "./purchase-file.json";
 
-/**
- * parse items into food & drink,
- * split them
- * get an array of objects
- * key values is customer id
- */
-
-type RawPurchaseDataType = {
-  customerId: string;
-  date: string;
-  items: string;
-};
-
-type FormattedDataUnit = {
-  customerId: Array<Purchase>;
-};
-
-type Purchase = {
-  date: string;
-  products: {
-    food: string[];
-    drinks: string[];
-  };
-};
-
 const parseData = () => {
-  const structuredData: any = {};
+  const structuredData: Record<string, FormattedDataUnit> = {};
 
   rawData.forEach((element: RawPurchaseDataType) => {
     const allItems = element.items.split(",");
     const food: Array<string> = [];
-    const drink: Array<string> = [];
+    const drinks: Array<string> = [];
 
     allItems.forEach((item: string) => {
       if (item.includes("FOOD")) {
         food.push(item);
       } else {
-        drink.push(item);
+        drinks.push(item);
       }
     });
-    const purchase = {
+    const purchase: Purchase = {
       date: element.date,
       products: {
         food,
-        drink,
+        drinks,
       },
     };
-
-    structuredData[element.customerId] = [purchase];
+    structuredData[element.customerId] = {
+      purchases: [purchase],
+    };
   });
   return structuredData;
 };
